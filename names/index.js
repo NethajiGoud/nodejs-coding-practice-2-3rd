cc966971d4968d66aaea0b6ce4ed7dfdfdcbc6b7
NethajiGoud <nethajigoud1999@gmail.com>
2022-10-10 17:28:57 +0000

    3rd

---
+++ names/index.js
@@ -0,0 +1,9 @@
+let a = require("../utilities/utils/index.js");
+const b = require("../country/state/city/index.js");
+
+function getPeopleInCity() {
+  let c = a(b);
+  //console.log(c);
+  return c;
+}
+module.exports = getPeopleInCity;
