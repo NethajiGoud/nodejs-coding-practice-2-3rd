3641f8429df858662731681c8909639ba4c75ff6
NethajiGoud <114430294+NethajiGoud@users.noreply.github.com>
2022-10-12 15:53:10 +0530

    Update index.js

--- names/index.js
+++ names/index.js
@@ -1,7 +1,7 @@
 let a = require("../utilities/utils/index.js");
 const b = require("../country/state/city/index.js");
 
-function getPeopleInCity() {
+function getPeopleInCity(b) {
   let c = a(b);
   //console.log(c);
   return c;
